f39b5646f99fcd8947b410f2e875ba21807f2e98
Rajat Subhra Ghosh <Rajat.Ghosh@oracle.com>
2021-04-15 08:48:17 -0700

    Started building the app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn How to build application using react
         </a>
       </header>
     </div>
