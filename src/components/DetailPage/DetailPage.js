e7305cc6f4dc9e1158bb83170d36ab7140d8fde2
krupa <krupamp231@gmail.com>
2021-05-17 19:42:55 -0700

    booking break fix

--- src/components/DetailPage/DetailPage.js
+++ src/components/DetailPage/DetailPage.js
@@ -43,6 +43,7 @@ function DetailPage(props) {
             const getCampDetails = await fetch(apiUrlForGetCampDetail)
             const result = await getCampDetails.json();
             var campSiteData = result["data"][0]
+            console.log('result', result)
             setCampDetail(campSiteData)
             setImages(getImageList(campSiteData["images"]))
             setCampSiteArea(getCamsiteDetail(campSiteData["campsites"]))
@@ -57,13 +58,16 @@ function DetailPage(props) {
     }
 
     const bookingHandler = (data) => {
-        console.log("data", data)
-        console.log('booking handler', data.userCount, data.startDate._d, data.endDate._d)
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (!user) {
+            alert('Please SignIn to book this site!!!')
+            return
+        }
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                "user_id": 1,
+                "user_id": user.id,
                 "park_code": parkCode,
                 "camp_ground_id": id,
                 "users_count": data.userCount,
@@ -72,110 +76,110 @@ function DetailPage(props) {
             })
         };
         fetch('/api/booking', requestOptions)
-          .then(response => response.json())
+            .then(response => response.json())
     }
 
     if (!isLoaded) {
         return <div>Loading....</div>
     } else {
         return (
-          <div className="camp_site_content">
-              {/* Imagesetup */}
-              <Swiper slidesPerView={1} navigation={true} pagination={{
-                  "clickable": true
-              }} className="mySwiper">
-                  {images.map((image, index) => {
-                      return <SwiperSlide key={`swiper-${index}`}><img src={image} /></SwiperSlide>;
-                  })}
-              </Swiper>
-              <CContainer>
-                  <CRow>
-                      <CCol md={8}>
-                          {/* Campsite Name */}
-                          <CRow>
-                              <CCol>
-                                  <h1 className="display-5">{campDetail.name}</h1>
-                                  <h5>Nearby:Angeles National Forest</h5>
-                              </CCol>
-                          </CRow>
-                          {/* Campsite Description */}
-                          <CRow>
-                              <CCol className="camp_border">
-                                  <p>{campDetail.description}</p>
-                                  <p>{campDetail.audioDescription}</p>
-                                  <p>{campDetail.reservationInfo}</p>
-                              </CCol>
-                          </CRow>
-                      </CCol>
-                      {/* DatePicker for reservation */}
-                      <CCol md={4}>
-                          <DatePicker fees={campDetail["fees"][0]["cost"]} bookingClick={bookingHandler} />
-                      </CCol>
-                  </CRow>
-              </CContainer>
-              <CContainer className="camp_padding">
-                  <CRow>
-                      <CCol md={8}>
-                          <CRow>
-                              <CCol>
-                                  <Card arrData={campSiteArea} />
-                              </CCol>
-                              <CCol>
-                                  <Card arrData={campAreaAminities} />
-                              </CCol>
-                              {/* <CCol>
+            <div className="camp_site_content">
+                {/* Imagesetup */}
+                <Swiper slidesPerView={1} navigation={true} pagination={{
+                    "clickable": true
+                }} className="mySwiper">
+                    {images.map((image, index) => {
+                        return <SwiperSlide key={`swiper-${index}`}><img src={image} /></SwiperSlide>;
+                    })}
+                </Swiper>
+                <CContainer>
+                    <CRow>
+                        <CCol md={8}>
+                            {/* Campsite Name */}
+                            <CRow>
+                                <CCol>
+                                    <h1 className="display-5">{campDetail.name}</h1>
+                                    <h5>Nearby:Angeles National Forest</h5>
+                                </CCol>
+                            </CRow>
+                            {/* Campsite Description */}
+                            <CRow>
+                                <CCol className="camp_border">
+                                    <p>{campDetail.description}</p>
+                                    <p>{campDetail.audioDescription}</p>
+                                    <p>{campDetail.reservationInfo}</p>
+                                </CCol>
+                            </CRow>
+                        </CCol>
+                        {/* DatePicker for reservation */}
+                        <CCol md={4}>
+                            <DatePicker fees={campDetail["fees"][0]["cost"]} bookingClick={bookingHandler} />
+                        </CCol>
+                    </CRow>
+                </CContainer>
+                <CContainer className="camp_padding">
+                    <CRow>
+                        <CCol md={8}>
+                            <CRow>
+                                <CCol>
+                                    <Card arrData={campSiteArea} />
+                                </CCol>
+                                <CCol>
+                                    <Card arrData={campAreaAminities} />
+                                </CCol>
+                                {/* <CCol>
                                     <Card arrData={campSiteArea} key='camp' />
                                 </CCol> */}
-                          </CRow>
-                      </CCol>
-                  </CRow>
-              </CContainer>
-              <CContainer>
-                  <CRow>
-                      <CCol md={8}>
-                          <CCard color='light'>
-                              <CCardHeader style={{ fontWeight: "bold" }}>{'Direction and Weather Info'}</CCardHeader>
-                              <CCardBody>
-                                  <CRow>
-                                      <CCol>
-                                          <CListGroup flush className="camp_list_bg" style={{ padding: 10 }}>
-                                              <CListGroupItem className="listItem">
-                                                  {<FontAwesomeIcon pull="left" icon={Icon.faDirections} />}
-                                                  {campDetail["directionsOverview"]}
-                                              </CListGroupItem>
-                                              <CListGroupItem className="listItem">
-                                                  {<FontAwesomeIcon pull="left" icon={Icon.faCloudSun} />}
-                                                  {campDetail["weatherOverview"]}
-                                              </CListGroupItem>
-                                          </CListGroup>
-                                      </CCol>
-                                  </CRow>
-                              </CCardBody>
-                          </CCard>
-                      </CCol>
-                  </CRow>
-              </CContainer>
-              <CContainer>
-                  <CRow>
-                      <CCol md={8} className="camp_border">
-                          <CRow>
-                              <CCol><p className="camp_text">Details</p></CCol>
-                              <CCol>
-                                  <CRow><p className="camp_text">Address:</p>{campDetail["addresses"][0]["line1"]}</CRow>
-                                  <CRow><p className="camp_text">City:</p>{campDetail["addresses"][0]["city"]}</CRow>
-                                  <CRow><p className="camp_text">State:</p>{campDetail["addresses"][0]["stateCode"]}</CRow>
-                              </CCol>
-                              <CCol>
-                                  <CRow><p className="camp_text">Phone Number:</p>{campDetail["contacts"]["phoneNumbers"][0]["phoneNumber"]}</CRow>
-                                  <CRow><p className="camp_text">Email:</p>{campDetail["contacts"]["emailAddresses"][0]["emailAddress"]}</CRow>
-                                  <CRow><p className="camp_text">Accepts bookings:</p>3 months out</CRow>
-                              </CCol>
-                          </CRow>
-                      </CCol>
-                  </CRow>
-              </CContainer>
-              <SmallCard arrActivities={campAreaActivites} key='campAct' />
-          </div>
+                            </CRow>
+                        </CCol>
+                    </CRow>
+                </CContainer>
+                <CContainer>
+                    <CRow>
+                        <CCol md={8}>
+                            <CCard color='light'>
+                                <CCardHeader style={{ fontWeight: "bold" }}>{'Direction and Weather Info'}</CCardHeader>
+                                <CCardBody>
+                                    <CRow>
+                                        <CCol>
+                                            <CListGroup flush className="camp_list_bg" style={{ padding: 10 }}>
+                                                <CListGroupItem className="listItem">
+                                                    {<FontAwesomeIcon pull="left" icon={Icon.faDirections} />}
+                                                    {campDetail["directionsOverview"]}
+                                                </CListGroupItem>
+                                                <CListGroupItem className="listItem">
+                                                    {<FontAwesomeIcon pull="left" icon={Icon.faCloudSun} />}
+                                                    {campDetail["weatherOverview"]}
+                                                </CListGroupItem>
+                                            </CListGroup>
+                                        </CCol>
+                                    </CRow>
+                                </CCardBody>
+                            </CCard>
+                        </CCol>
+                    </CRow>
+                </CContainer>
+                <CContainer>
+                    <CRow>
+                        <CCol md={8} className="camp_border">
+                            <CRow>
+                                <CCol><p className="camp_text">Details</p></CCol>
+                                <CCol>
+                                    <CRow><p className="camp_text">Address:</p>{campDetail["addresses"].length != 0 ? campDetail["addresses"][0]["line1"] : 'No Information'}</CRow>
+                                    <CRow><p className="camp_text">City:</p>{campDetail["addresses"].length != 0 ? campDetail["addresses"][0]["city"] : 'No Information'}</CRow>
+                                    <CRow><p className="camp_text">State:</p>{campDetail["addresses"].length != 0 ? campDetail["addresses"][0]["stateCode"] : 'No Information'}</CRow>
+                                </CCol>
+                                <CCol>
+                                    <CRow><p className="camp_text">Phone Number:</p>{campDetail["contacts"]["phoneNumbers"].length != 0 ? campDetail["contacts"]["phoneNumbers"][0]["phoneNumber"] : 'No Information'}</CRow>
+                                    <CRow><p className="camp_text">Email:</p>{campDetail["contacts"]["emailAddresses"].length != 0 ? campDetail["contacts"]["emailAddresses"][0]["emailAddress"] : 'No Information'}</CRow>
+                                    <CRow><p className="camp_text">Accepts bookings:</p>3 months out</CRow>
+                                </CCol>
+                            </CRow>
+                        </CCol>
+                    </CRow>
+                </CContainer>
+                <SmallCard arrActivities={campAreaActivites} key='campAct' />
+            </div>
         );
     }
 };
